cf18f5f24d522203c151ec7ba628a8ad1d772bc0
praajj <parteprajaktaa@gmail.com>
2025-01-17 10:43:43 +0530

    basic setup

--- IRCTC/settings.py
+++ IRCTC/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'IRCTC.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'IRCTC_DataBase',
+        'USER': 'root',
+        'PASSWORD': 'Root123#',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
